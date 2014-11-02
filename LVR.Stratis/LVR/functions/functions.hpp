d1af1d7477274beba3f465e3a4362672567d5354
David <davidw2340@gmail.com>
2014-11-02 03:39:58 -0900

    Added LVR Framework

---
+++ LVR.Stratis/LVR/functions/functions.hpp
@@ -0,0 +1,11 @@
+/* Register new vehicle init functions here - (see hunterInit below and fn_hunterInit.sqf) */
+
+class LVR
+{
+    class vehInitFncs
+    {
+        file = "LVR\functions";
+		class vehRespawn      	  {}; //main respawn function - Do Not Delete
+		class hunterInit          {}; // Example - Can Be Deleted 
+    };
+};  
---
+++ LVR.Stratis/LVR/functions/readme.txt
@@ -0,0 +1,3 @@
+Create as many fn_ files (functions) here as you like and register them into the functions.hpp for vehicle init usage (OPTIONAL) 
+See the usage comment atop fn_vehrespawn.sqf for more info.
+
